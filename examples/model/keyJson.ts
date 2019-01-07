331e99529adcc8ea303698633e92e12b24c7cb8f
tkow <t.kusama0613@outlook.jp>
2019-01-07 18:44:23 +0900

    exampleもできた

--- examples/model/keyJson.ts
+++ examples/model/keyJson.ts
@@ -1,3 +1,4 @@
 export default {
-  child: 'hoge'
+  child: 'hoge',
+  fuga: 'fuga'
 }
---
+++ examples/typings/key2union.d.ts
@@ -0,0 +1,2 @@
+declare type TKeys = "parent.child" | "parent.fuga";
+declare type TFunc = <T = any>(Tkeys: Tkeys, option: T) => string;
